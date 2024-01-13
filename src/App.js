ac7217c08b4150643399bc075dea72ce4eeca40e
Muhammad-Bilal-Vayani <143824973+Muhammad-Bilal-Vayani@users.noreply.github.com>
2024-01-13 14:34:23 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Signup_Form } from "./Component/Signup_Form";
+
+
+const App = () => {
+  
+  return (
+    <>
+     <Signup_Form />
+    </>
+  );
+};
+
+export default App;
